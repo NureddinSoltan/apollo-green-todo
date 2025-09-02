59562461b1a3c503307719d3f0c467dd8acecb0f
NureddinSoltan <noorsoltan40@gmail.com>
2025-09-03 02:10:39 +0300

    chore: add CorsMiddleware middleware  and SessionMiddleware

--- backend/src/config/settings.py
+++ backend/src/config/settings.py
@@ -60,11 +60,14 @@ INSTALLED_APPS = [
     "categories",
     "projects",
     "tasks",
-    "notifications",
+    # TODO: in the future
+    # "notifications",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
